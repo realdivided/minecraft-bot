1113a9751a505e1208cfe312fd210f15e3961ab5
realdivided <117919623+realdivided@users.noreply.github.com>
2023-07-14 18:48:04 +0200

    Add files via upload

---
+++ bot/bot.go
@@ -0,0 +1,42 @@
+package bot
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"github.com/realdivided/minecraft-bot/handlers"
+	"log"
+	"os"
+	"os/signal"
+)
+
+const (
+	Intents        = 1535
+	RemoveCommands = false
+)
+
+func StartBot(token, guildId string) {
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Panicf("Error creating Discord session: %v", err)
+	}
+
+	handlers.AddHandlers(session)
+	session.Identify.Intents = Intents
+
+	err = session.Open()
+	if err != nil {
+		log.Panicf("Error opening Discord session: %v", err)
+	}
+
+	handlers.CreateApplicationCommands(session, guildId)
+	log.Println("Bot is now running. Press CTRL-C to exit.")
+
+	defer session.Close()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	if RemoveCommands {
+		handlers.RemoveApplicationCommands(session)
+	}
+}
