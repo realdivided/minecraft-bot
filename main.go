d89cccb48d5c4405db9215ad4fb21bef633e9b19
realdivided <117919623+realdivided@users.noreply.github.com>
2023-07-14 18:36:25 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/realdivided/minecraft-bot/bot"
+	"github.com/realdivided/minecraft-bot/connection"
+	"log"
+	"os"
+)
+
+var (
+	Token        string
+	MongoUri     string
+	RconAddress  string
+	RconPassword string
+	GuildId      string
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Panicf("Error loading .env file: %v", err)
+	}
+
+	Token = os.Getenv("DISCORD_TOKEN")
+	MongoUri = os.Getenv("MONGODB_URI")
+	RconAddress = os.Getenv("RCON_ADDRESS")
+	RconPassword = os.Getenv("RCON_PASSWORD")
+	GuildId = os.Getenv("GUILD_ID")
+
+	log.Print("Successfully loaded .env file")
+}
+func main() {
+	connection.ImportConfiguration()
+	connection.ConnectMongo(MongoUri)
+	connection.ConnectRcon(RconAddress, RconPassword)
+	bot.StartBot(Token, GuildId)
+}
